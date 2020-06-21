943accdf7151b66be67af7516112c0ff9aec1d10
Nate Weaver <wevah@derailer.org>
2020-06-20 19:15:43 -0500

    Add 'vers' editor

---
+++ Cocoa/Plug-Ins/vers Editor/VersWindowController.swift
@@ -0,0 +1,12 @@
+//
+//  VersWindowController.swift
+//  ResKnife
+//
+//  Created by Nate Weaver on 2020-06-20.
+//
+
+import AppKit
+
+class VersWindowController: NSWindowController, ResKnifePluginProtocol {
+
+}
